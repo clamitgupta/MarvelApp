fcfb0a2cba93709784f39c98f4a7b404487707dc
clamitgupta <amitgupta@clerisysolutions.com>
2017-11-24 12:31:19 +0530

    updated comic-detail page

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # MarvelApp
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.3.
 
 Steps:
-1) git clone https://github.com/clamitgupta/MarvelApp.git.
-2) cd MarvelApp.
-3) npm install
-4) ng serve
+1) install angular-cli (npm install -g @angular/cli@latest)
+2) git clone https://github.com/clamitgupta/MarvelApp.git.
+3) cd MarvelApp.
+4) npm install
+5) ng serve
+6) Navigate to `http://localhost:4200/` in the browser and you will be able to run Test Marvel App
--- src/app/components/comic-detail/comic-detail.component.ts
+++ src/app/components/comic-detail/comic-detail.component.ts
@@ -8,17 +8,18 @@ import { HomeService } from "../../service/home.service";
   styleUrls: ['./comic-detail.component.css']
 })
 export class ComicDetailComponent implements OnInit {
-  public data: any = "loading........";
+  public data: any;
+  public showmore: boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     public homeService: HomeService
   ) {
-     this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe(params => {
       let id = params['id'];
       this.getComicDetail(id);
     });
-   }
+  }
 
   ngOnInit() {
   }
@@ -28,6 +29,38 @@ export class ComicDetailComponent implements OnInit {
       .then((resp: any) => {
         if (resp && resp.data && resp.data.results.length) {
           this.data = resp.data.results[0];
+          for (var prices of resp.data.results[0].prices) {
+            if (prices.type === "printPrice") {
+              this.data.price = prices.price;
+            }
+          }
+
+          for (var date of resp.data.results[0].dates) {
+            if (date.type === "focDate") {
+              this.data.focDate = date.date;
+            }
+          }
+
+          for (var creator of resp.data.results[0].creators.items) {
+            if (creator.role === "colorist (cover)") {
+              this.data.coverColorist = creator.name;
+            } else if (creator.role === "inker (cover)") {
+              this.data.coverInker = creator.name;
+            } else if (creator.role === "penciler (cover)") {
+              this.data.penciler = creator.name;
+            } else if (creator.role === "writer") {
+              this.data.writer = creator.name;
+            } else if (creator.role === "colorist") {
+              this.data.colorist = creator.name;
+            } else if (creator.role === "editor") {
+              this.data.editor = creator.name;
+            } else if (creator.role === "inker") {
+              this.data.inker = creator.name;
+            } if (creator.role === "letterer") {
+              this.data.letterer = creator.name;
+            }
+        }
+          
         } else {
           console.log(JSON.stringify("no reso"));
         }
@@ -35,6 +68,10 @@ export class ComicDetailComponent implements OnInit {
       .catch(err => {
         console.log(JSON.stringify(err));
       })
+  }
+
+  showMore(){
+    this.showmore = true;
   }
 
 }
